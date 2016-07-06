a0a2e271ec4a8333780593087b16c5a3d022d7f8
MaHuMapKo1 <leha13super@gmail.com>
2016-07-06 21:52:56 +0300

    ClientCard POJO changes.

--- app/src/main/java/com/mahumapko/smartumtask/POJO/ClientCard/Card.java
+++ app/src/main/java/com/mahumapko/smartumtask/POJO/ClientCard/Card.java
@@ -1,238 +1,276 @@
 
 package com.mahumapko.smartumtask.POJO.ClientCard;
 
-import java.util.HashMap;
-import java.util.Map;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import com.mahumapko.smartumtask.POJO.ClientCard.CardDto;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({
-    "id",
-    "cardDto",
-    "scoreBalance",
-    "name",
-    "cardCode",
-    "alreadyMaxDiscount",
-    "untilNextDiscount",
-    "nextDiscount",
-    "qrCodeToken"
-})
-public class Card {
+public class Card{
 
-    @JsonProperty("id")
+    @SerializedName("id")
+    @Expose
     private Integer id;
-    @JsonProperty("cardDto")
+    @SerializedName("cardDto")
+    @Expose
     private CardDto cardDto;
-    @JsonProperty("scoreBalance")
+    @SerializedName("scoreBalance")
+    @Expose
     private Integer scoreBalance;
-    @JsonProperty("name")
+    @SerializedName("name")
+    @Expose
     private String name;
-    @JsonProperty("cardCode")
+    @SerializedName("cardCode")
+    @Expose
     private String cardCode;
-    @JsonProperty("alreadyMaxDiscount")
+    @SerializedName("alreadyMaxDiscount")
+    @Expose
     private Boolean alreadyMaxDiscount;
-    @JsonProperty("untilNextDiscount")
+    @SerializedName("untilNextDiscount")
+    @Expose
     private Double untilNextDiscount;
-    @JsonProperty("nextDiscount")
+    @SerializedName("nextDiscount")
+    @Expose
     private Double nextDiscount;
-    @JsonProperty("qrCodeToken")
+    @SerializedName("qrCodeToken")
+    @Expose
     private String qrCodeToken;
-    @JsonIgnore
-    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
     /**
-     * 
+     * No args constructor for use in serialization
+     *
+     */
+    public Card() {
+    }
+
+    /**
+     *
+     * @param nextDiscount
+     * @param id
+     * @param scoreBalance
+     * @param untilNextDiscount
+     * @param name
+     * @param alreadyMaxDiscount
+     * @param qrCodeToken
+     * @param cardCode
+     * @param cardDto
+     */
+    public Card(Integer id, CardDto cardDto, Integer scoreBalance, String name, String cardCode, Boolean alreadyMaxDiscount, Double untilNextDiscount, Double nextDiscount, String qrCodeToken) {
+        this.id = id;
+        this.cardDto = cardDto;
+        this.scoreBalance = scoreBalance;
+        this.name = name;
+        this.cardCode = cardCode;
+        this.alreadyMaxDiscount = alreadyMaxDiscount;
+        this.untilNextDiscount = untilNextDiscount;
+        this.nextDiscount = nextDiscount;
+        this.qrCodeToken = qrCodeToken;
+    }
+
+    /**
+     *
      * @return
      *     The id
      */
-    @JsonProperty("id")
     public Integer getId() {
         return id;
     }
 
     /**
-     * 
+     *
      * @param id
      *     The id
      */
-    @JsonProperty("id")
     public void setId(Integer id) {
         this.id = id;
     }
 
+    public Card withId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
     /**
-     * 
+     *
      * @return
      *     The cardDto
      */
-    @JsonProperty("cardDto")
     public CardDto getCardDto() {
         return cardDto;
     }
 
     /**
-     * 
+     *
      * @param cardDto
      *     The cardDto
      */
-    @JsonProperty("cardDto")
     public void setCardDto(CardDto cardDto) {
         this.cardDto = cardDto;
     }
 
+    public Card withCardDto(CardDto cardDto) {
+        this.cardDto = cardDto;
+        return this;
+    }
+
     /**
-     * 
+     *
      * @return
      *     The scoreBalance
      */
-    @JsonProperty("scoreBalance")
     public Integer getScoreBalance() {
         return scoreBalance;
     }
 
     /**
-     * 
+     *
      * @param scoreBalance
      *     The scoreBalance
      */
-    @JsonProperty("scoreBalance")
     public void setScoreBalance(Integer scoreBalance) {
         this.scoreBalance = scoreBalance;
     }
 
+    public Card withScoreBalance(Integer scoreBalance) {
+        this.scoreBalance = scoreBalance;
+        return this;
+    }
+
     /**
-     * 
+     *
      * @return
      *     The name
      */
-    @JsonProperty("name")
     public String getName() {
         return name;
     }
 
     /**
-     * 
+     *
      * @param name
      *     The name
      */
-    @JsonProperty("name")
     public void setName(String name) {
         this.name = name;
     }
 
+    public Card withName(String name) {
+        this.name = name;
+        return this;
+    }
+
     /**
-     * 
+     *
      * @return
      *     The cardCode
      */
-    @JsonProperty("cardCode")
     public String getCardCode() {
         return cardCode;
     }
 
     /**
-     * 
+     *
      * @param cardCode
      *     The cardCode
      */
-    @JsonProperty("cardCode")
     public void setCardCode(String cardCode) {
         this.cardCode = cardCode;
     }
 
+    public Card withCardCode(String cardCode) {
+        this.cardCode = cardCode;
+        return this;
+    }
+
     /**
-     * 
+     *
      * @return
      *     The alreadyMaxDiscount
      */
-    @JsonProperty("alreadyMaxDiscount")
     public Boolean getAlreadyMaxDiscount() {
         return alreadyMaxDiscount;
     }
 
     /**
-     * 
+     *
      * @param alreadyMaxDiscount
      *     The alreadyMaxDiscount
      */
-    @JsonProperty("alreadyMaxDiscount")
     public void setAlreadyMaxDiscount(Boolean alreadyMaxDiscount) {
         this.alreadyMaxDiscount = alreadyMaxDiscount;
     }
 
+    public Card withAlreadyMaxDiscount(Boolean alreadyMaxDiscount) {
+        this.alreadyMaxDiscount = alreadyMaxDiscount;
+        return this;
+    }
+
     /**
-     * 
+     *
      * @return
      *     The untilNextDiscount
      */
-    @JsonProperty("untilNextDiscount")
     public Double getUntilNextDiscount() {
         return untilNextDiscount;
     }
 
     /**
-     * 
+     *
      * @param untilNextDiscount
      *     The untilNextDiscount
      */
-    @JsonProperty("untilNextDiscount")
     public void setUntilNextDiscount(Double untilNextDiscount) {
         this.untilNextDiscount = untilNextDiscount;
     }
 
+    public Card withUntilNextDiscount(Double untilNextDiscount) {
+        this.untilNextDiscount = untilNextDiscount;
+        return this;
+    }
+
     /**
-     * 
+     *
      * @return
      *     The nextDiscount
      */
-    @JsonProperty("nextDiscount")
     public Double getNextDiscount() {
         return nextDiscount;
     }
 
     /**
-     * 
+     *
      * @param nextDiscount
      *     The nextDiscount
      */
-    @JsonProperty("nextDiscount")
     public void setNextDiscount(Double nextDiscount) {
         this.nextDiscount = nextDiscount;
     }
 
+    public Card withNextDiscount(Double nextDiscount) {
+        this.nextDiscount = nextDiscount;
+        return this;
+    }
+
     /**
-     * 
+     *
      * @return
      *     The qrCodeToken
      */
-    @JsonProperty("qrCodeToken")
     public String getQrCodeToken() {
         return qrCodeToken;
     }
 
     /**
-     * 
+     *
      * @param qrCodeToken
      *     The qrCodeToken
      */
-    @JsonProperty("qrCodeToken")
     public void setQrCodeToken(String qrCodeToken) {
         this.qrCodeToken = qrCodeToken;
     }
 
-    @JsonAnyGetter
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    @JsonAnySetter
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
+    public Card withQrCodeToken(String qrCodeToken) {
+        this.qrCodeToken = qrCodeToken;
+        return this;
     }
 
 }
