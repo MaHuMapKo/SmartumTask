6d65670704820a811a1f460cf2d2176a726388be
MaHuMapKo1 <leha13super@gmail.com>
2016-07-12 16:50:48 +0300

    - added interface implemetation. - method getJsonString added.

--- app/src/main/java/com/mahumapko/smartumtask/JSONConverter.java
+++ app/src/main/java/com/mahumapko/smartumtask/JSONConverter.java
@@ -5,6 +5,7 @@ import android.content.res.AssetManager;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.mahumapko.smartumtask.Abstract.DataService;
 import com.mahumapko.smartumtask.POJO.ClientCard.Card;
 import com.mahumapko.smartumtask.POJO.MyPresents.Purchase;
 import com.mahumapko.smartumtask.POJO.MyPresents.Purchases;
@@ -15,116 +16,51 @@ import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.List;
 
-public class JSONConverter {
+public class JSONConverter implements DataService{
 
-    public static Card convertCardJson(Context context) {
-        AssetManager am = context.getResources().getAssets();
-        String json;
-        try {
-            InputStream is = am.open("clientCard.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-
-            json = new String(buffer, Charset.forName("UTF-8"));
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
+    public JSONConverter() {}
+    @Override
+    public Card getCard(Context context) {
+        String json = getJsonString(context, "clientCard.json");
         Gson gson = new GsonBuilder().create();
         Card card = gson.fromJson(json, Card.class);
         return card;
     }
 
-    public static List<String> getImagesFromPresents(Context context, List<String> images) {
-        AssetManager am = context.getResources().getAssets();
-        String json;
-        try {
-            InputStream is = am.open("presents.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-
-            json = new String(buffer, Charset.forName("UTF-8"));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
+    @Override
+    public List<Present> getPresents(Context context) {
+        String json = getJsonString(context, "presents.json");
         Gson gson = new GsonBuilder().create();
         Presents presents = gson.fromJson(json, Presents.class);
-        List<Present> list = presents.getPresents();
-        for (int i = 0; i < list.size(); i++) {
-            String imagePath = list.get(i).getImagePath();
-            if (!images.contains(imagePath))
-                images.add(imagePath);
-        }
-        return images;
+
+        return presents.getPresents();
     }
 
-    public static List<String> getImagesFromMyPresents(Context context, List<String> images) {
-        AssetManager am = context.getResources().getAssets();
-        String json;
-        try {
-            InputStream is = am.open("myPresents.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-
-            json = new String(buffer, Charset.forName("UTF-8"));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
+    @Override
+    public List<Purchase> getMyPresents(Context context) {
+        String json = getJsonString(context, "myPresents.json");
         Gson gson = new GsonBuilder().create();
         Purchases purchases = gson.fromJson(json, Purchases.class);
-        List<Purchase> list = purchases.getPurchases();
-        for (int i = 0; i < list.size(); i++) {
-            String imagePath = list.get(i).getPresent().getImagePath();
-            if (!images.contains(imagePath))
-                images.add(imagePath);
-        }
-        return images;
+
+        return purchases.getPurchases();
     }
 
-    public static Purchases getDataFromMyPresents(Context context) {
+    private static String getJsonString(Context context, String jsonFileName) {
         AssetManager am = context.getResources().getAssets();
         String json;
         try {
-            InputStream is = am.open("myPresents.json");
+            InputStream is = am.open(jsonFileName);
             int size = is.available();
             byte[] buffer = new byte[size];
             is.read(buffer);
             is.close();
 
             json = new String(buffer, Charset.forName("UTF-8"));
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-        Gson gson = new GsonBuilder().create();
-        return gson.fromJson(json, Purchases.class);
+        return json;
     }
 
-    public static Presents getDataFromPresents(Context context) {
-        AssetManager am = context.getResources().getAssets();
-        String json;
-        try {
-            InputStream is = am.open("presents.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-
-            json = new String(buffer, Charset.forName("UTF-8"));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        Gson gson = new GsonBuilder().create();
-        return gson.fromJson(json, Presents.class);
-    }
 }
