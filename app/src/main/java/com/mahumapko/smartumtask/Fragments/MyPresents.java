c25ca00169e460d45841d6dbd36419002451a242
MaHuMapKo1 <leha13super@gmail.com>
2016-07-13 16:59:44 +0300

    - added recyclerView to MyPresents. - added RecyclerView and ShopDescriptionAdapter to shopDescription.class(renamed from AboutUsFragment)

--- app/src/main/java/com/mahumapko/smartumtask/Fragments/MyPresents.java
+++ app/src/main/java/com/mahumapko/smartumtask/Fragments/MyPresents.java
@@ -2,11 +2,11 @@ package com.mahumapko.smartumtask.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 import com.mahumapko.smartumtask.adapters.MyPresentsAdapter;
 import com.mahumapko.smartumtask.JSONConverter;
@@ -18,7 +18,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyPresents extends Fragment {
-    MyPresentsAdapter adapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,23 +26,16 @@ public class MyPresents extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View root = (View) inflater.inflate(R.layout.my_presents_fragment, container, false);
+        View root = (View) inflater.inflate(R.layout.presents_fragment, container, false);
 
-        ListView list = (ListView) root.findViewById(R.id.my_presents_list);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                DevelopDialog dialog = new DevelopDialog();
-                dialog.show(getFragmentManager(), "DialogFragment");
-            }
-        });
+        RecyclerView recycler = (RecyclerView) root.findViewById(R.id.recycler);
 
-        createAdapter(list);
+        createAdapter(recycler);
 
         return root;
     }
 
-    private void createAdapter(ListView listView) {
+    private void createAdapter(RecyclerView recycler) {
         List<Purchase> list = new JSONConverter().getMyPresents(getActivity());
 
         List<String> images = new ArrayList<>();
@@ -57,8 +49,8 @@ public class MyPresents extends Fragment {
             scoreCount.add(present.getScore());
         }
 
-        adapter = new MyPresentsAdapter(getActivity(), R.layout.presents_item,
-                images, names, scoreCount);
-        listView.setAdapter(adapter);
+        MyPresentsAdapter adapter = new MyPresentsAdapter(getActivity(), images, names, scoreCount);
+        recycler.setAdapter(adapter);
+        recycler.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
 }
